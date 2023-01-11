47d365690c7942de4ddf6ea425e9cbb5c9b5db12
JA-Lourenco <ja.lourenco@outlook.com>
2023-01-11 20:35:58 -0300

    Adjusting loading behavior

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,19 +1,12 @@
-import { Loading } from "../Loading";
 import { ImgContainer, A, Image, Video, DescBox, Description } from "./styled";
 
 interface ImageCardProps {
   source: string;
   description: string;
   type?: string;
-  isLoading?: boolean;
 }
 
-export const ImageCard = ({
-  source,
-  description,
-  type,
-  isLoading,
-}: ImageCardProps) => {
+export const ImageCard = ({ source, description, type }: ImageCardProps) => {
   if (description.length > 20) {
     description = `${description.substring(0, 20)}...`;
   }
@@ -32,18 +25,12 @@ export const ImageCard = ({
 
   return (
     <ImgContainer>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <A href={source} target="_blank">
-            <ImageOrVideo />
-          </A>
-          <DescBox>
-            <Description>{description}</Description>
-          </DescBox>
-        </>
-      )}
+      <A href={source} target="_blank">
+        <ImageOrVideo />
+      </A>
+      <DescBox>
+        <Description>{description}</Description>
+      </DescBox>
     </ImgContainer>
   );
 };
